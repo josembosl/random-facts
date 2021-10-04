e8af29c6c8b865b3f2d0401ca6196dca98896757
Jose Belo <josembosl@gmail.com>
2021-10-04 16:36:06 +0100

    added test

--- components/simple.test.js
+++ components/simple.test.js
@@ -5,3 +5,9 @@ describe('sum()', () => {
     expect(sum(2, 3)).toBe(5)
   })
 })
+
+describe('sum2()', () => {
+    it('should return 5 if given 2 and 3 ', () => {
+      expect(sum(2, 2)).toBe(5)
+    })
+  })
