d90353d598b8e15769214089ab8b6e04944d517d
José Maria Belo <josembosl@gmail.com>
2021-10-08 09:26:12 +0100

    fixed "" bug

--- pages/api/FactsHandler.test.js
+++ pages/api/FactsHandler.test.js
@@ -1,9 +1,12 @@
 import getRandomFact from './FactsHandler';
+import * as axios from 'axios';
 
 jest.mock("axios");
 
 describe("getRandomFact Test", () => {
     it("should call axios.get", async () => {
+        
+
          await getRandomFact();
 
         expect(axios.get).toHaveBeenCalled();
